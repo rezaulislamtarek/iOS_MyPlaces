6b2bc631c0a891952ed465a87c04cfa8860665d8
rezaulislamtarek <mrezaulislam.t@gmail.com>
2023-02-23 10:03:32 +0600

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Rezaul Islam on 23/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
